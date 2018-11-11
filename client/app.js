2a5c928402bc5a0ef4e4025270687bf0250f0c1d
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-11-10 22:03:03 -0500

    boilerplate for single page app

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Routes from './routes';
+import { Home } from './components';
 
 const App = () => {
   return (
     <div>
-      <Routes />
+      <Home />
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/api', require('./api'))
-
 app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 })
