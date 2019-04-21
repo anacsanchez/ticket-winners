f5c2ea6a1a9d788da000e74b01c66ce175c3a138
Ana C Sanchez <ana@anacsanchez.com>
2019-04-21 12:11:13 -0400

    combined entrant and winner list with highlight for winners

--- client/components/UserList.js
+++ client/components/UserList.js
@@ -3,11 +3,14 @@ import React from 'react';
 const UserList = ({ entrants, winners }) => (
   <div className="user-lists">
     {
-      entrants.map(user =>
-        <div className="entrants" key={`user-${user.id}`}>
-          Id: {user.id}, Tickets: {user.tickets}
-        </div>
-      )
+      entrants.map(entrant => {
+        const isWinner = winners.find(user => user.id == entrant.id)
+        return (
+          <div className={`users ${isWinner ? 'winner' : null }`} key={`user-${entrant.id}`}>
+            Email: {entrant.email}, Tickets: {entrant.tickets}
+          </div>
+        );
+      })
     }
   </div>
 );
