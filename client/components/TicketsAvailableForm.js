3310b5eec3660cde13f5f5570ed4aa97047c976a
Ana C Sanchez <ana@anacsanchez.com>
2019-04-21 20:52:37 -0400

    changed submit button value to pick winners

--- client/components/TicketsAvailableForm.js
+++ client/components/TicketsAvailableForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TicketForm = ({ handleSubmit }) => (
   <form className="small-forms" onSubmit={handleSubmit}>
     <div><label>Tickets Available: </label><input type="text" name="ticketsAvailable" size="2" required /></div>
-    <input type="submit" value="Submit" className="input-submit-buttons"/>
+    <input type="submit" value="Pick Winners" className="input-submit-buttons"/>
   </form>
 );
 
